92dce82d1581b1a86a77c663e2551a2bcb6a5186
Dimitar <dimitar_dimitrov12@mail.bg>
2020-09-10 12:45:28 +0300

    Updates

---
+++ precache-manifest.a3db0462b0bb1742ff650b30acb164d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "056f390bf4d6cf11b03febf05498621e",
+    "url": "/To-Do-App/index.html"
+  },
+  {
+    "revision": "b14c1e992b678cecb9c9",
+    "url": "/To-Do-App/static/css/main.597547a3.chunk.css"
+  },
+  {
+    "revision": "6de636f165175ed94e56",
+    "url": "/To-Do-App/static/js/2.53a0c092.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-App/static/js/2.53a0c092.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b14c1e992b678cecb9c9",
+    "url": "/To-Do-App/static/js/main.616d162e.chunk.js"
+  },
+  {
+    "revision": "969a3dcc5f0ad23ce880",
+    "url": "/To-Do-App/static/js/runtime-main.c6765458.js"
+  }
+]);
